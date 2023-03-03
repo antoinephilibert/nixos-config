f2c864e0eaa7f2783e2268360cb4c22d6d6d85e4
Antoine Philibert <antoine.philibert@praxar.com>
2023-03-03 18:12:06 -0500

    Adding Plasma 5

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -34,9 +34,19 @@
         device = "/dev/sda";                    # Name of hard drive (can also be vda)
       };
       timeout = 10;                              # Grub auto select timeout
-
     };
+  };
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
   };
 }
